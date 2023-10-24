2022ad444136d08c6d361c794caae359fb9f782b
Aprecity <kathrynfisher3700@gmail.com>
2023-10-23 20:22:04 -0400

    added README basic file structure

--- index.js
+++ index.js
@@ -1,35 +1,85 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { title } = require('process');
 
 inquirer
   .prompt([
     {
       type: 'input',
-      message: 'What is your name?',
-      name: 'first',
+      message: 'What is the title of your project?',
+      name: 'title',
     },
     {
       type: 'input',
-      message: 'Where are you located?',
-      name: 'location',
+      message: 'Describe your project. What solutions does it solve?',
+      name: 'description',
     },
     {
       type: 'input',
-      message: 'What is a fun fact about you?',
-      name: 'funFact',
+      message: 'Describe any installation requirements.',
+      name: 'installation',
     },
     {
       type: 'input',
-      message: 'What is your LinkedIn username or link?',
-      name: 'linkedIn',
+      message: 'Describe how your application is used.',
+      name: 'usage',
     },
+    {
+      type: 'input',
+      message: 'List any contributors and sources.',
+      name: 'contributions',
+    },
+    {
+      type: 'input',
+      message: 'What type of licensing?',
+      name: 'license',
+    },
+    {
+      type: 'input',
+      message: 'List any test instructions.',
+      name: 'test',
+    },
+
+
   ])
   .then((response) => {
-    const {name, location, funFact, linkedIn} = response
+    const {title, description, installation, usage, contributions, license, test} = response
+
     fs.writeFile('README.md', 
-    "example"
+    `# ${title}
+
+    ## Description
+    ${description}
+
+    ## Table of Contents
+      [Installation](installation)
+      [Usage](usage)
+      [License](license)
+      [Contributing](contributing)
+      [Tests](tests)
+      
+    ## Installation
+    ${installation}
+    
+    ## Usage
+    ${usage}
+    ##License
+    ${license}
+    ##Contributing
+    ${contributions}
+    #Tests
+    ${test}
+    ##Questions
+    
+    
+    `
 
     , (err) => err ? console.error(err) : console.log('Thank You!'))
   }
   );
+
+  function init() {}
+
+// Function call to initialize app
+init();
   
